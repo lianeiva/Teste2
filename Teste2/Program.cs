acf319861747a20cc84d34d468753f9765444acb
Marcelo Neiva <marcelo.lianeiva@hotmail.com>
2020-01-16 15:50:44 -0300

    1º linha adicionada

--- Teste2/Program.cs
+++ Teste2/Program.cs
@@ -7,6 +7,7 @@ namespace Teste2
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Teste 2 1º Linha");
         }
     }
 }
